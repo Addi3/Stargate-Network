be8d4b4456793333676b86d466b22c05a4a5b2c2
Addi3 <122154427+Addi3@users.noreply.github.com>
2025-10-07 19:25:25 +0100

    init commit

---
+++ src/client/java/com/addie/StargateNetworkClient.java
@@ -0,0 +1,10 @@
+package com.addie;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StargateNetworkClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
