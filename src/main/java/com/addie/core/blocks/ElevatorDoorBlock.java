4bd19216351d4470b81687d3d69f49a08e89539b
Addi3 <122154427+Addi3@users.noreply.github.com>
2025-10-14 19:31:37 +0100

    chore: elevator door rotational voxel shape

--- src/main/java/com/addie/core/blocks/ElevatorDoorBlock.java
+++ src/main/java/com/addie/core/blocks/ElevatorDoorBlock.java
@@ -8,13 +8,17 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
@@ -22,39 +26,62 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ElevatorDoorBlock extends Block implements BlockEntityProvider  {
+public class ElevatorDoorBlock extends HorizontalFacingBlock implements BlockEntityProvider {
+
     public static final BooleanProperty OPEN = BooleanProperty.of("open");
-	public  ElevatorDoorBlock(Settings settings) {
+    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+
+    public ElevatorDoorBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(OPEN, false));
+        this.setDefaultState(this.stateManager.getDefaultState()
+                .with(FACING, Direction.NORTH)
+                .with(OPEN, false));
     }
 
-    protected static final VoxelShape SHAPE= Block.createCuboidShape(
-            -16, 0, 0, 32, 48, 16
-    );
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return this.getDefaultState()
+                .with(FACING, context.getHorizontalPlayerFacing().getOpposite())
+                .with(OPEN, false);
+    }
+
+    protected static final VoxelShape SHAPE_NORTH = Block.createCuboidShape(-16, 0, 0, 32, 48, 16);
+    protected static final VoxelShape SHAPE_SOUTH = Block.createCuboidShape(-16, 0, 0, 32, 48, 16);
+    protected static final VoxelShape SHAPE_EAST  = Block.createCuboidShape(0, 0, -16, 16, 48, 32);
+    protected static final VoxelShape SHAPE_WEST  = Block.createCuboidShape(0, 0, -16, 16, 48, 32);
+
+    private static VoxelShape getShapeForDirection(Direction direction) {
+        return switch (direction) {
+            case NORTH -> SHAPE_NORTH;
+            case SOUTH -> SHAPE_SOUTH;
+            case EAST  -> SHAPE_EAST;
+            case WEST  -> SHAPE_WEST;
+            default    -> SHAPE_NORTH;
+        };
+    }
 
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return state.get(OPEN) ? VoxelShapes.empty() : SHAPE;
+        return state.get(OPEN) ? VoxelShapes.empty() : getShapeForDirection(state.get(FACING));
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return SHAPE;
+        return getShapeForDirection(state.get(FACING));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(OPEN);
+        builder.add(FACING, OPEN);
     }
 
     @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+    public ActionResult onUse(BlockState state, World world, BlockPos pos,
+                              PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (world.getBlockEntity(pos) instanceof ElevatorDoorBlockEntity be) {
 
-            if (!world.isClient) { // Server-side only
+            if (!world.isClient) {
                 boolean isOpen = state.get(ElevatorDoorBlock.OPEN);
-
                 world.setBlockState(pos, state.with(ElevatorDoorBlock.OPEN, !isOpen), Block.NOTIFY_ALL);
 
                 world.playSound(
@@ -66,7 +93,6 @@ public class ElevatorDoorBlock extends Block implements BlockEntityProvider  {
                         1.0f
                 );
             }
-
 
             be.useOn(world, player.isSneaking(), player);
             return ActionResult.SUCCESS;
@@ -86,8 +112,10 @@ public class ElevatorDoorBlock extends Block implements BlockEntityProvider  {
     }
 
     @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull World world, @NotNull BlockState state,
-                                                                  @NotNull BlockEntityType<T> type) {
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(
+            @NotNull World world,
+            @NotNull BlockState state,
+            @NotNull BlockEntityType<T> type) {
         return (world1, blockPos, blockState, ticker) -> {
             if (ticker instanceof ElevatorDoorBlockEntity be) {
                 be.tick(world, blockPos, blockState, be);
@@ -95,4 +123,3 @@ public class ElevatorDoorBlock extends Block implements BlockEntityProvider  {
         };
     }
 }
-
